8cdee25dabe1654c669acf79ff778052fd3ed859
Joanna <m.joanna.foss@gmail.com>
2021-12-16 14:46:27 -0600

    completed: home; in-prog: math

---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,12 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add")
+    @ResponseBody
+    public String standin(){return ".";}
+}
